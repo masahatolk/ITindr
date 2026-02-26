010fc6473771254ef8f00803ee0b12b4afb08a8e
Tolkacheva Maria <141151654+masahatolk@users.noreply.github.com>
2026-02-26 19:41:18 +0700

    Подключила линтер

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,20 @@
+apply {
+    plugin(rootProject.libs.plugins.detekt.get().pluginId)
+}
+detekt {
+    autoCorrect = true
+    config.setFrom("$rootDir/config/detekt/detekt.yml")
+}
+dependencies {
+    detektPlugins(rootProject.libs.detekt.formatting)
+    detektPlugins(rootProject.libs.detekt.rules.compose)
+    detekt(rootProject.libs.detekt.cli)
+}
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.detekt)
 }
 
 android {
@@ -38,6 +52,7 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
